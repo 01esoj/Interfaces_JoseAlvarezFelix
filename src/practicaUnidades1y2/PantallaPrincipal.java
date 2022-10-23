cb8b4e1acd0136f6e2bb636ea73f36ff43f6ce88
senor <senor@LAPTOP-7B428IIP>
2022-10-23 18:51:27 +0200

    Ejemplo contenido pantalla principal

--- src/practicaUnidades1y2/PantallaPrincipal.java
+++ src/practicaUnidades1y2/PantallaPrincipal.java
@@ -1,33 +1,50 @@
 package practicaUnidades1y2;
 
+import java.io.IOException;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 
 public class PantallaPrincipal extends Application{
+	
+	private Stage primaryStage;
 	private BorderPane rootLayout;
 
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			// Carga el diseño del archivo FXML en la variable rootLayout
-			FXMLLoader loader = new FXMLLoader();
-			loader.setLocation(PantallaPrincipal.class.getResource("PantallaPrincipal.fxml"));
-			rootLayout = (BorderPane) loader.load();
-
-			// Mostramos la escena del BorderPane de la variable rootLayot
-			Scene scene = new Scene(rootLayout);
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			this.primaryStage = primaryStage;
+			initRootLayout();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	public void initRootLayout() {
+		try {
+			// Carga el XML con el diseño principal
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(PantallaPrincipal.class.getResource("/view/PantallaPrincipal.fxml"));
+			rootLayout = (BorderPane) loader.load();
+			// Se añade el diseño principal a la escena
+			Scene scene = new Scene(rootLayout);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	public Stage getPrimaryStage() {
+		return primaryStage;
 	}
 }
