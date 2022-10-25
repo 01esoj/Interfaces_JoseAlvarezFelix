cc150a1431ef7e08c98912803227c0917ad7cf9e
joseAF <senor@LAPTOP-7B428IIP>
2022-10-25 13:53:40 +0200

    TreeView Completado

--- src/view/ControllerApp.java
+++ src/view/ControllerApp.java
@@ -5,6 +5,8 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.control.TitledPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
@@ -20,11 +22,75 @@ public class ControllerApp {
 	@FXML
     private TitledPane paginaContacto;
 	
+	@FXML
+    private TreeView<String> tree1;
+	
 	private BorderPane rootLayout;
 	
 	@FXML
     private void initialize() {
+		TreeItem<String> series = new TreeItem<String>("");
 		
+		TreeItem<String> serieA = new TreeItem<String>("series A");
+    	
+    	TreeItem<String> serieA1 = new TreeItem<String>("A1");
+    	TreeItem<String> serieA3 = new TreeItem<String>("A3");
+    	TreeItem<String> serieA4 = new TreeItem<String>("A4");
+    	TreeItem<String> serieA5 = new TreeItem<String>("A5");
+    	TreeItem<String> serieA6 = new TreeItem<String>("A6");
+    	TreeItem<String> serieA7 = new TreeItem<String>("A7");
+    	TreeItem<String> serieA8 = new TreeItem<String>("A8");
+    	
+    	serieA.getChildren().add(serieA1);
+    	serieA.getChildren().add(serieA3);
+    	serieA.getChildren().add(serieA4);
+    	serieA.getChildren().add(serieA5);
+    	serieA.getChildren().add(serieA6);
+    	serieA.getChildren().add(serieA7);
+    	serieA.getChildren().add(serieA8);
+    	
+    	TreeItem<String> serieQ = new TreeItem<String>("series Q");
+    	
+    	TreeItem<String> serieQ2 = new TreeItem<String>("Q2");
+    	TreeItem<String> serieQ3 = new TreeItem<String>("Q3");
+    	TreeItem<String> serieQ4 = new TreeItem<String>("Q4 e-tron");
+    	TreeItem<String> serieQ5 = new TreeItem<String>("Q5");
+    	TreeItem<String> serieQ7 = new TreeItem<String>("Q7");
+    	TreeItem<String> serieQ8 = new TreeItem<String>("Q8");
+    	
+    	serieQ.getChildren().add(serieQ2);
+    	serieQ.getChildren().add(serieQ3);
+    	serieQ.getChildren().add(serieQ4);
+    	serieQ.getChildren().add(serieQ5);
+    	serieQ.getChildren().add(serieQ7);
+    	serieQ.getChildren().add(serieQ8);
+    	
+    	TreeItem<String> otrasSeries = new TreeItem<String>("Otras series");
+    	
+    	TreeItem<String> eTronGT = new TreeItem<String>("e-tron GT");
+    	TreeItem<String> eTron = new TreeItem<String>("e-tron");
+    	TreeItem<String> TT = new TreeItem<String>("TT");
+    	TreeItem<String> R8 = new TreeItem<String>("R8");
+    	TreeItem<String> RS = new TreeItem<String>("RS");
+    	TreeItem<String> S = new TreeItem<String>("S");
+    	
+    	otrasSeries.getChildren().add(eTronGT);
+    	otrasSeries.getChildren().add(eTron);
+    	otrasSeries.getChildren().add(TT);
+    	otrasSeries.getChildren().add(R8);
+    	otrasSeries.getChildren().add(RS);
+    	otrasSeries.getChildren().add(S);
+    	
+    	series.getChildren().add(serieA);
+    	series.getChildren().add(serieQ);
+    	series.getChildren().add(otrasSeries);
+    	
+    	series.setExpanded(true);
+    	tree1.setRoot(series);
+    	
+    	serieA.setExpanded(false);
+    	serieQ.setExpanded(false);
+    	otrasSeries.setExpanded(false);
 	}
 	
 	@FXML
