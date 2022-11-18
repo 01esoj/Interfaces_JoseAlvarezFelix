dfc7f9f577471eed37bd44dc3496f328d773763d
01esoj <101212109+01esoj@users.noreply.github.com>
2022-11-18 01:12:16 +0100

    Add files via upload

--- Jose_Alvarez_Felix_PracticaUnidades3/src/view/ControllerFormulario.java
+++ Jose_Alvarez_Felix_PracticaUnidades3/src/view/ControllerFormulario.java
@@ -1,6 +1,5 @@
 package view;
 
-import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
@@ -10,7 +9,6 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
-import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.control.Alert.AlertType;
 
